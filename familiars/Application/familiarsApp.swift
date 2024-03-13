41a829b9d6cfe072e9a6e3116729ded3128a63d4
Junho Yu <95111999+ryuwon2407@users.noreply.github.com>
2024-03-14 01:40:42 +0900

    Feat: 프로젝트 MVVM 세팅

---
+++ familiars/Application/familiarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  familiarsApp.swift
+//  familiars
+//
+//  Created by 준호 on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct familiarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
