e396648e1edd4a769cbdb90f02416694139116a3
Junho Yu <95111999+ryuwon2407@users.noreply.github.com>
2024-03-15 11:18:35 +0900

    Feat: 프로젝트 MVVM 세팅

---
+++ Familiars/Application/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Familiars
+//
+//  Created by 준호 on 3/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Hellooooooo")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Familiars/Application/FamiliarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamiliarsApp.swift
+//  Familiars
+//
+//  Created by 준호 on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FamiliarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
