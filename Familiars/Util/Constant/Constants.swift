5baf4633101d08fd8288916efe151c0100348f16
Junho Yu <95111999+ryuwon2407@users.noreply.github.com>
2024-03-15 17:44:10 +0900

    Feat: Constants 추가 - Color

---
+++ Familiars/Util/Constant/Constants.swift
@@ -0,0 +1,41 @@
+//
+//  Constant.swift
+//  Familiars
+//
+//  Created by 준호 on 3/15/24.
+//
+
+import SwiftUI
+
+/// Constants - 공통으로 사용되는 상수 값 관리
+struct Constants {
+    // MARK: - padding
+    
+    // MARK: - Font
+    
+    // MARK: - Color
+    static let main100 = Color(red: 0.84, green: 0.85, blue: 0.99)
+    static let main200 = Color(red: 0.55, green: 0.55, blue: 0.99)
+    static let main300 = Color(red: 0.44, green: 0.44, blue: 0.79)
+    static let main400 = Color(red: 0.31, green: 0.32, blue: 0.69)
+    
+    static let grayScale50 = Color(red: 0.97, green: 0.97, blue: 0.98)
+    static let grayScale100 = Color(red: 0.95, green: 0.95, blue: 0.96)
+    static let grayScale200 = Color(red: 0.91, green: 0.91, blue: 0.93)
+    static let grayScale300 = Color(red: 0.84, green: 0.85, blue: 0.88)
+    static let grayScale400 = Color(red: 0.71, green: 0.71, blue: 0.74)
+    static let grayScale500 = Color(red: 0.6, green: 0.6, blue: 0.62)
+    static let grayScale600 = Color(red: 0.44, green: 0.44, blue: 0.46)
+    static let grayScale700 = Color(red: 0.36, green: 0.36, blue: 0.38)
+    static let grayScale800 = Color(red: 0.25, green: 0.25, blue: 0.26)
+    static let grayScale900 = Color(red: 0.12, green: 0.13, blue: 0.13)
+    
+    static let error: Color = Color(red: 0.99, green: 0.28, blue: 0.28)
+    
+    // MARK: - URL 주소
+    
+    // MARK: - 애니메이션 시간 (스플래쉬 등)
+    
+    // MARK: - 공용 컴포넌트
+    
+}
